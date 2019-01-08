869f61e2e350717b647fb547ec8268294733f3e2
sutter <david.sutter@here.com>
2019-01-07 17:59:22 -0800

    added a config file

--- server.js
+++ server.js
@@ -1,42 +1,54 @@
-//This is a static file web server
-//note -add some sort of mime types? Now I just leave it blank
+//This is a static file web server, based of the apogee server code
+//note - add some sort of mime types? Now I just leave it blank
 var http = require("http");
 var parser = require('url');
+var fs = require('fs');
 var filehandler = require("./filehandler");
 
-//===========================================================
-// SETTINGS
-const FILE_ROOT = "../ApogeeJS/web/";
-const CONTEXT_ROOT = "/"
-const PORT = 8888;
-//===========================================================
+const CONFIG_FILE = "config.json";
 
-//create a static file handler
-var fileHandler = filehandler.createInstance(FILE_ROOT);
+
 
 //--------------------------------
 // start server
 //--------------------------------
 
-route = function(request,response) {
-    var url = parser.parse(request.url,true);
-
-    var pathname = url.pathname;
-
-    //make sure the context root matches
-    if(!pathname.startsWith(CONTEXT_ROOT)) {
-        response.writeHead(code, {"Content-Type":"text/plain"});
-        response.write(msg);
-        response.end();
-        return;
+function onLoadConfig(err,configText) {
+    if(err) {
+        console.log(err.msg);
+        response.writeHead(500, {"Content-Type":"text/plain"});
+        response.write("Error!");
     }
+    else {
+        //start the server
+        var config = JSON.parse(configText);
 
-    //path for handle should not include leading '/'
-    var childPath = pathname.substring(CONTEXT_ROOT.length);
-    var queryString = url.search;
+        //create a static file handler
+        var fileHandler = filehandler.createInstance(config.fileRoot);
 
-    fileHandler.process(childPath,queryString,request,response);
+        route = function(request,response) {
+            var url = parser.parse(request.url,true);
+
+            var pathname = url.pathname;
+
+            //make sure the context root matches
+            if(!pathname.startsWith(config.contextRoot)) {
+                response.writeHead(code, {"Content-Type":"text/plain"});
+                response.write(msg);
+                response.end();
+                return;
+            }
+
+            //path for handle should not include leading '/'
+            var childPath = pathname.substring(config.contextRoot.length);
+            var queryString = url.search;
+
+            fileHandler.process(childPath,queryString,request,response);
+        }
+
+        //create listener
+        http.createServer(route).listen(config.port);
+    }
 }
 
-//create listener
-http.createServer(route).listen(PORT);
+fs.readFile(CONFIG_FILE,onLoadConfig);
